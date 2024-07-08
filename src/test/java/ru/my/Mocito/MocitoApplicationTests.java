94e028affbea9fac69f6a98d806b9384a0b78179
HomeMedia <taknado13@yandex.ru>
2024-07-09 03:57:31 +0700

    start

---
+++ src/test/java/ru/my/Mocito/MocitoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.my.Mocito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MocitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
